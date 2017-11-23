72743e3f0ad4c40bdc097a8bcbb4fe83272dff05
Ainhoa Calviño <ainhoa.c.rodriguez@gmail.com>
2017-11-23 21:42:14 +0100

    add color interdictor

---
+++ viperTest/Modules/Coordinator/ColorCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  ColorCoordinator.swift
+//  viperTest
+//
+//  Created by Ainhoa Calviño on 23/11/17.
+//  Copyright © 2017 Ainhoa. All rights reserved.
+//
+
+import UIKit
+
+class ColorCoordinator: UIColor {
+    public class func forkColor() -> UIColor {
+        return UIColor(red:0.90, green:0.83, blue:0.94, alpha:1.0)
+    }
+    
+    public class func backgroundGray() -> UIColor {
+        return UIColor(red:0.94, green:0.94, blue:0.94, alpha:1.0)
+    }
+}
