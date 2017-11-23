b50d0e3cb3b637734f59bb83f3ba2848469415cc
Ainhoa Calviño <ainhoa.c.rodriguez@gmail.com>
2017-11-23 21:43:11 +0100

    implement router to show list of repositories

---
+++ viperTest/Modules/ListRepos/Router/Router.swift
@@ -0,0 +1,43 @@
+//
+//  Router.swift
+//  viperTest
+//
+//  Created by Ainhoa Calviño on 11/11/17.
+//  Copyright © 2017 Ainhoa. All rights reserved.
+//
+
+import UIKit
+
+class Router: NSObject, RouterProtocol {
+    
+    //build objects
+    let vc: ViewController = ViewController()
+    let presenter = Presenter()
+    let interactor = Interactor()
+    let dataManager = DataManager()
+    var navigationController: UINavigationController?
+    
+    override init() {
+        super.init()
+        
+        vc.presenter = presenter
+        presenter.view = vc
+        presenter.interactor = interactor
+        presenter.router = self
+        interactor.dataManager = dataManager
+        dataManager.interactor = presenter
+        navigationController = UINavigationController(rootViewController: vc)
+    }
+    
+    func presentInterfaceFromWindow(window: UIWindow) {
+        vc.presenter = presenter
+        presenter.view = vc
+        presenter.interactor = interactor
+        presenter.router = self
+        interactor.dataManager = dataManager
+        dataManager.interactor = presenter
+        navigationController = UINavigationController(rootViewController: vc)
+        
+        window.rootViewController = self.navigationController
+    }
+}
---
+++ viperTest/Modules/ListRepos/Router/RouterProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  RouterProtocol.swift
+//  viperTest
+//
+//  Created by Ainhoa Calviño on 11/11/17.
+//  Copyright © 2017 Ainhoa. All rights reserved.
+//
+
+import UIKit
+
+protocol RouterProtocol: class {
+
+    func presentInterfaceFromWindow(window: UIWindow)
+}
